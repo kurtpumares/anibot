fe5a3a608c03cb77ec9fb63590c33623ed0a83d6
Kurt Pumares <kurt.pumares@ezswitch.com.au>
2020-07-24 19:54:19 +0800

    Transferred get message command away from API file

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 import logging
-from random import choice
 
 from flask import Flask, request
 from config import VERIFY_TOKEN
-from jikanpy import Jikan
 
-from messenger import RandomAnimeBot
+from messenger import get_message, RandomAnimeBot
 from error import InvalidMethodError
 
 
 app = Flask(__name__)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-
-VERIFY_TOKEN = VERIFY_TOKEN
-MAX_GENRE_COUNT = 43
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -51,38 +45,6 @@ def verify_fb_token(tokenSent):
         return request.args.get('hub.challenge')
     else:
         return 'invalid verification token'
-
-
-def get_message():
-        meta = get_random_from_100()
-
-        title = meta['title']
-        genres = meta['genres']
-        image_url = meta['image_url']
-
-        message = f'Title: {title}\nGenres: {', '.join(genres)}'
-
-        return message, image_url
-
-
-def get_random_from_100():
-    jikan = Jikan()
-
-    result = jikan.genre(type='anime', genre_id=choice(range(MAX_GENRE_COUNT)))
-    anime_meta = result['anime'][choice(range(99))]
-    genres = []
-
-    for i in range(len(anime_meta['genres'])):
-        genres.append(anime_meta['genres'][i]['name'])
-
-    meta_data = dict(title=anime_meta['title'], genres=genres,
-                     mal_id=anime_meta['mal_id'], url=anime_meta['url'],
-                     synopsis=anime_meta['synopsis'],
-                     image_url=anime_meta['image_url'],
-                     score=anime_meta['score'], type=anime_meta['type'],
-                     airing_start=anime_meta['airing_start'])
-
-    return meta_data
 
 
 if __name__ == '__main__':
--- messenger.py
+++ messenger.py
@@ -1,8 +1,43 @@
+from random import choice
+
 from config import ACCESS_TOKEN
 from pymessenger.bot import Bot
+from jikanpy import Jikan
 
 
-ACCESS_TOKEN = ACCESS_TOKEN
+MAX_GENRE_COUNT = 43
+
+
+def get_message():
+    meta = get_random_from_100()
+
+    title = meta['title']
+    genres = meta['genres']
+    image_url = meta['image_url']
+
+    message = f'Title: {title}\nGenres: {', '.join(genres)}'
+
+    return message, image_url
+
+
+def get_random_from_100():
+    jikan = Jikan()
+
+    result = jikan.genre(type='anime', genre_id=choice(range(MAX_GENRE_COUNT)))
+    anime_meta = result['anime'][choice(range(99))]
+    genres = []
+
+    for i in range(len(anime_meta['genres'])):
+        genres.append(anime_meta['genres'][i]['name'])
+
+    meta_data = dict(title=anime_meta['title'], genres=genres,
+                     mal_id=anime_meta['mal_id'], url=anime_meta['url'],
+                     synopsis=anime_meta['synopsis'],
+                     image_url=anime_meta['image_url'],
+                     score=anime_meta['score'], type=anime_meta['type'],
+                     airing_start=anime_meta['airing_start'])
+
+    return meta_data
 
 
 class RandomAnimeBot:
